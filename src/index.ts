2b2df0efd745b92e1c6ee2670166e13cbdbdf15e
Andrew Moss <andrew@m0ss.dev>
2021-02-13 16:49:50 -0700

    including necessary exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-export * from "./Helmet";
-export * from "./HelmetConstants";
-export * from "./HelmetUtils"
+import HelmetExport from "./Helmet";
+
+export { Helmet } from "./Helmet";
+
+export {
+    convertReactPropstoHtmlAttributes,
+    handleClientStateChange,
+    mapStateOnServer,
+    reducePropsToState,
+    requestAnimationFrame,
+    warn
+} from "./HelmetUtils"
+
+
+export {
+    ATTRIBUTE_NAMES,
+    TAG_NAMES,
+    VALID_TAG_NAMES,
+    TAG_PROPERTIES,
+    REACT_TAG_MAP,
+    HELMET_PROPS,
+    HTML_TAG_MAP,
+    SELF_CLOSING_TAGS,
+    HELMET_ATTRIBUTE
+} from "./HelmetConstants";
