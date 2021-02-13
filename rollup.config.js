300bd58ebc815777c6baa068eab376a7ab34daf0
Andrew Moss <andrew@m0ss.dev>
2021-02-13 16:56:33 -0700

    Helmet file as index

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default {
-    input: "src/index.ts",
+    input: "src/index.tsx",
     output: [
         {
             dir: "dist",
